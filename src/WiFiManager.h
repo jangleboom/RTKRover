b2057dc6bb87bf93863b29b58a22123719c7b9e7
Markus Hädrich <markus.haedrich@tu-berlin.de>
2022-04-04 16:36:09 +0200

    Button2 class added, more gestures and functionalities are callable for future, debouncing done by Button2 object itself

--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,3 +1,17 @@
+
+/**
+ * @file WiFiManager.h
+ * @author based on Roland Pelayo, refactored by Markus Hädrich
+ * @link https://www.teachmemicro.com/esp32-wifi-manager-dynamic-ssid-password
+ * <br>
+ * @brief This is part of a distributed software, here: head tracker and GNNS positioning
+ * using Sparkfun Real Time Kinematics
+ * <br>
+ * @todo  - a lot
+ * @note no "Ä,ä, Ö, ö, Ü, ü" or other strange letters as SSID and PW allowed 
+ */
+
+
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,73 @@
 
+/**
+ * @file main.cpp
+ * @authors Markus Hädrich && Thomas Resch 
+ * <br>
+ * @brief This is part of a distributed software, here: head tracker and GNNS positioning
+ * using Sparkfun Real Time Kinematics
+ * <br>
+ * @todo  - a lot
+ */
+
 #include "WiFiManager.h"
 
-// Button to press to wipe out stored wifi credentials
-#define WIFI_CRED_RESET_PIN 15 
-#define DELAY_MS 1000
-#define BAUD 115200
+const int DELAY_MS = 1000;
 
+/******************************************************************************/
+//                        Serial settings
+/******************************************************************************/
+//set to true for debug output, false for no debug output
+#define DEBUGGING true 
+#define DEBUG_SERIAL \
+  if (DEBUGGING) Serial
+
+const int BAUD = 115200;
+
+/******************************************************************************/
+//                                Button(s)
+/******************************************************************************/
+#include "Button2.h"
+// Button to press to wipe out stored wifi credentials
+const int BUTTON_PIN = 15;
+Button2 button = Button2(BUTTON_PIN, INPUT, false, false);
+
+void buttonHandler(Button2 &btn);
+
+
+void buttonHandler(Button2 &btn) {
+  if (btn == button) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    DEBUG_SERIAL.println("button clicked");
+    DEBUG_SERIAL.println("Wiping WiFi credentials from memory...");
+    wipeEEPROM();
+    while (loadWIFICredsForm()) {};
+  }
+}
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(WIFI_CRED_RESET_PIN, INPUT); //for resetting WiFi creds
-  EEPROM.begin(400);
   Serial.begin(BAUD);
+  while (!Serial) {};
+  EEPROM.begin(400);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  button.setPressedHandler(buttonHandler); // INPUT_PULLUP is set too here
+
   if (!CheckWIFICreds()) {
-    Serial.println("No WIFI credentials stored in memory. Loading form...");
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("No WIFI credentials stored in memory. Loading form...");
     while (loadWIFICredsForm());
   }
 }
 
 void loop() {
-  if(digitalRead(WIFI_CRED_RESET_PIN) == HIGH) {
-    Serial.println("Wiping WiFi credentials from memory...");
-    wipeEEPROM();
-    while (loadWIFICredsForm());
-  }
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(DELAY_MS);
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(DELAY_MS);
+  button.loop();
+  // if(digitalRead(BUTTON_PIN) == HIGH) {
+  //   Serial.println("Wiping WiFi credentials from memory...");
+  //   wipeEEPROM();
+  //   while (loadWIFICredsForm());
+  // }
+  // digitalWrite(LED_BUILTIN,HIGH);
+  // delay(DELAY_MS);
+  // digitalWrite(LED_BUILTIN,LOW);
+  // delay(DELAY_MS);
 }
