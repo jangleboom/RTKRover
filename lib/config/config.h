66da71325e6895c1691b1f6762b320ecaecac3bc
Markus Hädrich <markus.haedrich@mail.de>
2022-07-22 22:33:24 +0200

    RTK_REFRESH_INTERVAL_MS   to    20

--- lib/config/config.h
+++ lib/config/config.h
@@ -81,7 +81,7 @@ BUT: we use here two I2C connections for real parallel computing on two cores.
 #define GNSS_OVER_WIFI_PRIORITY       2  // GNSS should have a lower priority
 #define BNO080_OVER_BLE_PRIORITY      1  // Headtracking: highest priority
 #define BLE_TASK_INTERVAL_MS          10
-#define RTK_REFRESH_INTERVAL_MS       20 // Recomended is 1000 (1s)
+#define RTK_REFRESH_INTERVAL_MS       20 
 #define WIFI_TASK_INTERVAL_MS         100
 #define MIN_ACCEPTABLE_ACCURACY_MM    100 // Device will only send if accuray is better than this
 /*******************************************************************************
