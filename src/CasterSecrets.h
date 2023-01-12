fa507229f6db4da896f9cdcdb336894a7c3f84a0
Markus Hädrich <markus.haedrich@mail.de>
2023-01-12 19:20:47 +0100

    CasterSecrets.h updated

--- src/CasterSecrets.h
+++ src/CasterSecrets.h
@@ -10,10 +10,10 @@ const char kMountPoint[] = "soundwalk";
 const char kCasterUser[] = "fhnwbasel@gmail.com";   // User must provide their own email address to use RTK2Go
 const char kCasterUserPw[] = "";                    // This can be added to the web form if you want to use a PW here, it's not neccecary
 
-// Device name (this rover with headtracking)
-const char kDeviceName[] = "rtkrover123";
+// Device name 
+const char kDeviceName[] = "rover123";           
 // Wifi access
-const char kWifiSsid[] = "ssid_without_spaces";
-const char kWifiPw[] = "***";
+const char kWifiSsid[] = "YOUR_SSID_WITHOUT_SPACES"; // Wifi to connect the rover with
+const char kWifiPw[] = "YOUR_WIFI_PASSWORD";
 
 #endif /*** CASTER_SECRETS_H ***/
