b7b48f4d3e5595334587f9a19748a398910020c5
Markus Hädrich <markus.haedrich@mail.de>
2023-01-20 12:04:22 +0100

    CasterSecrets.h updated

--- src/CasterSecrets.h
+++ src/CasterSecrets.h
@@ -1,11 +1,14 @@
-#ifndef CASTER_SECRETS_H
-#define CASTER_SECRETS_H
+
 /** A place for your caster credentials.
- * Except of the empty kCasterUserPw[], this will be not used if 
+ * 
+ * Except of the (empty) kCasterUserPw[], this will be not used if 
  * you use the web form of the RTKRoverManager for credential input.
  * 
  * Check if your base station is online: http://www.rtk2go.com:2101/SNIP::STATUS#uptime
  */ 
+
+#ifndef CASTER_SECRETS_H
+#define CASTER_SECRETS_H
 
 const char kCasterHost[] = "rtk2go.com"; 
 const char kCasterPort[] = "2101";
