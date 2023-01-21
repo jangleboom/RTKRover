ddb961c4935f88ee47d6d43462b6952794c394db
Markus Hädrich <markus.haedrich@mail.de>
2023-01-21 14:30:42 +0100

    enableARVRStabilizedGameRotationVector for BNO080 addded

--- src/main.cpp
+++ src/main.cpp
@@ -766,7 +766,8 @@ void setupBNO080()
     
   // Activate IMU functionalities
   // bno080.enableRotationVector(BNO080_ROT_VECT_UPDATE_RATE_MS);  
-  // bno080.enableGameRotationVector(BNO080_ROT_VECT_UPDATE_RATE_MS);  
+  // bno080.enableGameRotationVector(BNO080_ROT_VECT_UPDATE_RATE_MS); 
+  //bno080.enableARVRStabilizedGameRotationVector(BNO080_ROT_VECT_UPDATE_RATE_MS); 
   bno080.enableARVRStabilizedRotationVector(BNO080_ROT_VECT_UPDATE_RATE_MS);   
 
   bno080.enableAccelerometer(BNO080_LIN_ACCEL_UPDATE_RATE_MS);    
