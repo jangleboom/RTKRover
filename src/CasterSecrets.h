8ca7b96f4bdbf8ab21fed4775ca3acb2d356e997
Markus Hädrich <markus.haedrich@mail.de>
2023-01-25 15:25:49 +0100

    Submodule RTKRoverManager updated

--- src/CasterSecrets.h
+++ src/CasterSecrets.h
@@ -2,18 +2,22 @@
 #define CASTER_SECRETS_H
 // A place for your caster credentials
 
-// RTK2Go http://www.rtk2go.com:2101/SNIP::STATUS#uptime
-
+//RTK2Go MountPoint 1 http://www.rtk2go.com:2101/SNIP::STATUS#uptime
+// Email: mr.markuese@gmail.com
 const char kCasterHost[] = "rtk2go.com"; 
 const char kCasterPort[] = "2101";
-const char kMountPoint[] = "soundwalk";
-const char kCasterUser[] = "fhnwbasel@gmail.com";   // User must provide their own email address to use RTK2Go
-const char kCasterUserPw[] = "";                    // This can be added to the web form if you want to use a PW here, it's not neccecary
-
-// Device name 
-const char kDeviceName[] = "rover123";           
-// Wifi access
-const char kWifiSsid[] = "YOUR_SSID_WITHOUT_SPACES"; // Wifi to connect the rover with
-const char kWifiPw[] = "YOUR_WIFI_PASSWORD";
-
+const char kCasterUserPw[] = "";                    
+const char kDeviceName[] = "rtkrover";
+const char kMountPoint[] = "rtkberlin";
+const char kCasterUser[] = "mr.markuese@gmail.com"; 
+// const char kWifiSsid[] = "Loewenzahn";
+// const char kWifiPw[] = "vollverschluesselt1312!";
+const char kWifiSsid[] = "moinmoin";
+const char kWifiPw[] = "test1234";
+// const char kWifiSsid[] = "1ra11a1a";
+// const char kWifiPw[] = "test1234";
 #endif /*** CASTER_SECRETS_H ***/
+
+
+
+
